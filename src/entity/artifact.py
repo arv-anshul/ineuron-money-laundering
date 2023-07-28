30d0308e2c03d2d091475058f16e39e81d2622e1
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-07-28 20:52:16 +0530

    Add src.entity

---
+++ src/entity/artifact.py
@@ -0,0 +1,44 @@
+""" Entity for artifact folder to store all data and models. """
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class DataIngestionArtifact:
+    base_data_path: Path
+    train_path: Path
+    test_path: Path
+
+
+# Maybe DataValidationArtifact is not required because it doesn't do with anything.
+@dataclass
+class DataValidationArtifact(DataIngestionArtifact):
+    report_fp: Path
+
+
+@dataclass
+class DataTransformationArtifact:
+    transformer_path: Path
+    target_enc_path: Path
+    train_arr_path: Path
+    test_arr_path: Path
+
+
+@dataclass
+class ModelTrainerArtifact:
+    model_path: Path
+    train_score: float
+    test_score: float
+
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted: bool
+    improved_accuracy: float
+
+
+@dataclass
+class ModelPusherArtifact:
+    dir_: Path
+    trained_model_dir: Path
---
+++ src/entity/config.py
@@ -0,0 +1,93 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from src.core import Config
+from src.core.constants import BASE_DATA_NAME
+
+run_id = Config.get_run_id()
+
+
+class PipelineConfig:
+    def __init__(self):
+        self.root = Path.cwd()
+        self.artifact_dir = Path('artifacts', run_id)
+        self.__create_all_dirs()
+
+    def __create_all_dirs(self):
+        self.artifact_dir.mkdir(parents=True, exist_ok=True)
+
+
+class DataIngestionConfig(PipelineConfig):
+    def __init__(self):
+        super().__init__()
+        self.base_data_path = self.root / 'data' / BASE_DATA_NAME
+        self.dir = self.artifact_dir / 'data_ingestion'
+        self.train_path = self.dir / 'train.parquet'
+        self.test_path = self.dir / 'test.parquet'
+        self.test_size = None   # According to project
+        self.__create_all_dirs()
+
+    def __create_all_dirs(self):
+        self.dir.mkdir(parents=True, exist_ok=True)
+
+
+class DataValidationConfig(DataIngestionConfig):
+    def __init__(self):
+        super().__init__()
+        self.dir = self.artifact_dir / 'data_validation'
+        self.reports_dir = self.root / 'reports'
+        self.drift_report_path = self.reports_dir / 'drift_report.yaml'
+        self.missing_threshold = None   # According to project
+        self.__create_all_dirs()
+
+    def __create_all_dirs(self):
+        self.dir.mkdir(exist_ok=True)
+        self.reports_dir.mkdir(exist_ok=True)
+
+
+class DataTransformationConfig(DataIngestionConfig):
+    def __init__(self):
+        super().__init__()
+        self.dir = self.artifact_dir / 'data_transformation'
+        self.transformer_pkl_path = self.dir / 'transformer.pkl'
+        self.target_enc_path = self.dir / 'target_encoder.pkl'
+        self.train_arr_path = self.dir / 'transformed_arrays/train.npy'
+        self.test_arr_path = self.dir / 'transformed_arrays/test.npy'
+        self.__create_all_dirs()
+
+    def __create_all_dirs(self):
+        self.dir.mkdir(exist_ok=True)
+        self.train_arr_path.parent.mkdir(parents=True, exist_ok=True)
+
+
+class ModelTrainerConfig(PipelineConfig):
+    def __init__(self):
+        super().__init__()
+        self.dir = self.artifact_dir / 'model_trainer'
+        self.model_path = self.dir / 'model.pkl'
+        self.expected_training_score = None   # According to project
+        self.expected_testing_score = None   # According to project
+        self.overfitting_threshold = None   # According to project
+        self.__create_all_dirs()
+
+    def __create_all_dirs(self):
+        self.dir.mkdir(exist_ok=True)
+
+
+@dataclass
+class ModelEvaluationConfig:
+    model_score_diff_threshold = None   # According to project
+
+
+class ModelPusherConfig(PipelineConfig):
+    def __init__(self):
+        super().__init__()
+        self.dir = self.artifact_dir / 'model_pusher'
+        self.model_path = self.dir / 'model.pkl'
+        self.transformer_path = self.dir / 'transformer.pkl'
+        self.target_enc_path = self.dir / 'target_encoder.pkl'
+        self.trained_model_dir = self.root / 'trained_models'
+        self.__create_all_dirs()
+
+    def __create_all_dirs(self):
+        self.dir.mkdir(exist_ok=True)
